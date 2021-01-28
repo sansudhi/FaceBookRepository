c7a1dae8596ba95dfabc668d1baaeed49ef932b3
user <user@DESKTOP-G46L397>
2021-01-28 16:08:28 +0530

    Comments removed

--- MyFramework/src/generic/FrameWork_Constants.java
+++ MyFramework/src/generic/FrameWork_Constants.java
@@ -8,7 +8,7 @@ public interface FrameWork_Constants
 String Gecko_Key="webdriver.gecko.driver";
 String Gecko_Value="./Softwares/geckodriver.exe";
 
-//String Chrome_Key="webdriver.chrome.driver";
-//String Chrome_Value="./Softwares/chromedriver.exe";
+String Chrome_Key="webdriver.chrome.driver";
+String Chrome_Value="./Softwares/chromedriver.exe";
 	
 }
